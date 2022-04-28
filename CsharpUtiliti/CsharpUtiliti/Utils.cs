e472028a47bf26b1ad890851b3f8afc3f94c24d0
HoangNguyen <wallker.night@gmail.com>
2022-04-28 14:36:07 +0700

    update

--- CsharpUtiliti/CsharpUtiliti/Utils.cs
+++ CsharpUtiliti/CsharpUtiliti/Utils.cs
@@ -1,10 +1,5 @@
 ﻿using Microsoft.VisualBasic;
-using System;
-using System.Collections.Generic;
 using System.Data;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace CsharpUtiliti
 {
--- CsharpUtiliti/EntityFrameworkUlti/CustomSorting.cs
+++ CsharpUtiliti/EntityFrameworkUlti/CustomSorting.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
+﻿using System.Linq.Expressions;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace EntityFrameworkUlti
 {
@@ -42,6 +37,7 @@ namespace EntityFrameworkUlti
                             && method.GetParameters().Length == 2)
                     .MakeGenericMethod(typeof(T), type)
                     .Invoke(null, new object[] { source, lambda });
+
             return (IOrderedQueryable<T>)result;
         }
     }
